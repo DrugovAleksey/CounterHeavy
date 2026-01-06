90c2da716b9d2466f100bd9314a8a1712318d8ec
Aleksey_Drugov <lexa_drugov@mail.ru>
2026-01-06 22:20:30 +0300

    Initial Commit

---
+++ CounterHeavy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CounterHeavy
+//
+//  Created by Flymetric on 06.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
